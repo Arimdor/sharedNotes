b603173fdf9709639ae97e6fae3397d8e2d2bf92
Arimdor <rodrigo.120894@gmail.com>
2018-05-09 19:07:40 -0500

    update

--- app/src/main/java/com/arimdor/sharednotes/utils/Constants.kt
+++ app/src/main/java/com/arimdor/sharednotes/utils/Constants.kt
@@ -2,7 +2,7 @@ package com.arimdor.sharednotes.utils
 
 class Constants {
     companion object {
-        const val BASE_URL = "http://192.168.1.50:8080/api/"
+        const val BASE_URL = "http://35.185.90.101/api/"
         const val TYPE_TEXT = 0
         const val TYPE_MULTIMEDIA = 1
         const val REQUEST_IMAGE_CAPTURE = 1
